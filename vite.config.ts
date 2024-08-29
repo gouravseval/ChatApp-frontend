baa1dad415573d89705340b2dbe3a52d49bca4d3
Gourav Jangra <104681594+gouravseval@users.noreply.github.com>
2024-08-30 00:30:55 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://chatapp-backend-6v42.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    }
-  }
 })
