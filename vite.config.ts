8cea48ea8ee2d7968cbe09cbb8b195ad9e7023d7
Gourav Jangra <104681594+gouravseval@users.noreply.github.com>
2024-08-30 14:13:13 +0530

    ef

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'https://chatapp-backend-6v42.onrender.com/',
         changeOrigin: true,
         secure: true,
       },
